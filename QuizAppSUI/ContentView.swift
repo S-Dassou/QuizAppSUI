9fa10d83ac4ce23121586e03129f9e2c4948813e
Shafique Dassu <shafd1231@gmail.com>
2023-08-05 23:48:27 +0100

    Initial Commit

---
+++ QuizAppSUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QuizAppSUI
+//
+//  Created by shafique dassu on 04/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizAppSUI/QuizAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppSUIApp.swift
+//  QuizAppSUI
+//
+//  Created by shafique dassu on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
