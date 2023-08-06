e597ff760208f7c461c23801b8990a5a27c0614a
Shafique Dassu <shafd1231@gmail.com>
2023-08-06 20:18:25 +0100

    Implemented button to change score when the correct button is tapped Created alert pop up when correct/incorrect button tapped Applied State and Binding variables for score and alert

--- QuizAppSUI/ContentView.swift
+++ QuizAppSUI/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var score = 0
+    @State var presentAlert = false
+    @State var alertTitle = ""
+    @State var alertMessage = ""
+    
     var body: some View {
         VStack {
             Image("lion")
@@ -17,52 +23,88 @@ struct ContentView: View {
             
             HStack{
                 Spacer()
-                Text("Score")
+                Text("Score = \(score)")
                     .font(.system(size: 16, weight: .semibold))
                     .padding(.trailing, 20)
                     .padding(.top, 15)
             }
             
-            Button {
-            } label: {
-                Text("Giraffe")
-                    .font(.system(size: 15, weight: .semibold))
-                    .foregroundColor(Color.white)
-                    .frame(maxWidth: .infinity)
-            }
-            .frame(height: 45)
-            .background(Color.green)
-            .cornerRadius(8)
-            .padding(.horizontal, 25)
-            .padding(.top, 30)
+            QuizButton(title: "Giraffe", presentAlert: $presentAlert, handler: {
+                alertTitle = "Quiz Answer"
+                alertMessage = "You got the wrong answer"
+               
+            })
+                .padding(.top, 30)
             
-            Button {
-            } label: {
-                Text("Lion")
-                    .font(.system(size: 15, weight: .semibold))
-                    .foregroundColor(Color.white)
-                    .frame(maxWidth: .infinity)
-            }
-            .frame(height: 45)
-            .background(Color.green)
-            .cornerRadius(8)
-            .padding(.horizontal, 25)
-            .padding(.top, 10)
+            QuizButton(title: "Lion", presentAlert: $presentAlert, handler: {
+                alertTitle = "Quiz Answer"
+                alertMessage = "You got the right answer"
+               
+                score += 1
+            })
             
-            Button {
-            } label: {
-                Text("Buffalo")
-                    .font(.system(size: 15, weight: .semibold))
-                    .foregroundColor(Color.white)
-                    .frame(maxWidth: .infinity)
-            }
-            .frame(height: 45)
-            .background(Color.green)
-            .cornerRadius(8)
-            .padding(.horizontal, 25)
-            .padding(.top, 10)
+            QuizButton(title: "Buffalo", presentAlert: $presentAlert, handler: {
+                alertTitle = "Quiz Answer"
+                alertMessage = "You got the wrong answer"
+                
+            })
+            
+//            Button {
+//                alertTitle = "Quiz Answer"
+//                alertMessage = "You got the wrong answer"
+//                presentAlert = true
+//            } label: {
+//                Text("Giraffe")
+//                    .font(.system(size: 15, weight: .semibold))
+//                    .foregroundColor(Color.white)
+//                    .frame(maxWidth: .infinity)
+//            }
+//            .frame(height: 45)
+//            .background(Color.green)
+//            .cornerRadius(8)
+//            .padding(.horizontal, 25)
+//            .padding(.top, 30)
+//
+//            Button {
+//                alertTitle = "Quiz Answer"
+//                alertMessage = "You got the right answer"
+//                presentAlert = true
+//                score += 1
+//            } label: {
+//                Text("Lion")
+//                    .font(.system(size: 15, weight: .semibold))
+//                    .foregroundColor(Color.white)
+//                    .frame(maxWidth: .infinity)
+//            }
+//            .frame(height: 45)
+//            .background(Color.green)
+//            .cornerRadius(8)
+//            .padding(.horizontal, 25)
+//            .padding(.top, 10)
+//
+//            Button {
+//                alertTitle = "Quiz Answer"
+//                alertMessage = "You got the wrong answer"
+//                presentAlert = true
+//            } label: {
+//                Text("Buffalo")
+//                    .font(.system(size: 15, weight: .semibold))
+//                    .foregroundColor(Color.white)
+//                    .frame(maxWidth: .infinity)
+//            }
+//            .frame(height: 45)
+//            .background(Color.green)
+//            .cornerRadius(8)
+//            .padding(.horizontal, 25)
+//            .padding(.top, 10)
             
         }
+        .alert(alertTitle, isPresented: $presentAlert) {
+            Button("OK", action: { })
+        } message: {
+            Text(alertMessage)
+        }
+
         
     }
 }
@@ -70,5 +112,29 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct QuizButton: View {
+    
+    let title: String
+    @Binding var presentAlert: Bool
+    var handler: () -> Void
+    
+    var body: some View {
+        Button {
+            handler()
+            presentAlert = true
+        
+        } label: {
+            Text(title)
+                .font(.system(size: 15, weight: .semibold))
+                .foregroundColor(Color.white)
+                .frame(maxWidth: .infinity)
+        }
+        .frame(height: 45)
+        .background(Color.green)
+        .cornerRadius(8)
+        .padding(.horizontal, 25)
     }
 }
